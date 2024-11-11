3961cd1293eca390df112c2e5ef533922a09faa1
James Dale Abeyta <abeyternater@Jamess-Air.lan>
2024-11-10 19:30:57 -0800

    deployment attempt 2

--- .netlify/v1/functions/ssr/.netlify/build/entry.mjs
+++ .netlify/v1/functions/ssr/.netlify/build/entry.mjs
@@ -1,6 +1,6 @@
 import * as adapter from '@astrojs/netlify/ssr-function.js';
 import { renderers } from './renderers.mjs';
-import { manifest } from './manifest_0676a2bb.mjs';
+import { manifest } from './manifest_7231549a.mjs';
 
 const _page0  = () => import('./chunks/generic_6e56fcba.mjs');
 const _page1  = () => import('./chunks/index_188ba4a7.mjs');
@@ -9,7 +9,7 @@ const _manifest = Object.assign(manifest, {
 	pageMap,
 	renderers,
 });
-const _args = {"middlewareSecret":"449daf4d-5758-4fbf-81c7-bfdef9cf7b00"};
+const _args = {"middlewareSecret":"a9bd565c-f1d2-444b-883f-f3aeccde1c7a"};
 
 const _exports = adapter.createExports(_manifest, _args);
 const _default = _exports['default'];
